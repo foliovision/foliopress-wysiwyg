73f9366711311bfcae6af46265e33956c54a06d1
fvmartin <martinv@foliovision.com>
2013-05-18 12:20:12 +0200

    Fix for FV Flowplayer button, fix for PHP warnings.

--- foliopress-wysiwyg-class.php
+++ foliopress-wysiwyg-class.php
@@ -5,7 +5,7 @@
  * Main class that handles all implementation of plugin into WordPress. All WordPress actions and filters are handled here
  *  
  * @author Foliovision s.r.o. <info@foliovision.com>
- * @version 2.6.8.2
+ * @version 2.6.8.3
  * @package foliopress-wysiwyg
  */
 
